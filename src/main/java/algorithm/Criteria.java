ab9712feaf005b9eecdcc0efe961a3526cc4943a
Ricard Osorio <celtric@gmail.com>
2017-03-18 08:43:59 +0100

    change switch to polymorphism inside Criteria

--- src/main/java/algorithm/Criteria.java
+++ src/main/java/algorithm/Criteria.java
@@ -1,25 +1,27 @@
 package algorithm;
+
 public enum Criteria {
-	One, Two;
-
-	Result calculate(Result lastResult, Result nextResult) {
-
-		switch (this) {
-			case One :
-				if (nextResult.ageDifference() < lastResult.ageDifference()) {
-					return nextResult;
-				} else {
-					return lastResult;
-				}
-
-			case Two :
-				if (nextResult.ageDifference() > lastResult.ageDifference()) {
-					return nextResult;
-				} else  {
-					return lastResult;
-				}
-			default:
-				throw new RuntimeException("Not implemented");
+	One {
+		@Override
+		Result calculate(Result lastResult, Result nextResult) {
+			if (nextResult.ageDifference() < lastResult.ageDifference()) {
+				return nextResult;
+			} else {
+				return lastResult;
+			}
 		}
-	}
+	},
+
+	Two {
+		@Override
+		Result calculate(Result lastResult, Result nextResult) {
+			if (nextResult.ageDifference() > lastResult.ageDifference()) {
+				return nextResult;
+			} else  {
+				return lastResult;
+			}
+		}
+	};
+
+	abstract Result calculate(Result lastResult, Result nextResult);
 }
